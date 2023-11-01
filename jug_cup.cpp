d53aa35e091712e3799807d6ea678ad6626d6072
P-4243 <148359317+P-4243@users.noreply.github.com>
2023-11-01 16:20:11 +0530

    Update jug_cup.cpp

--- jug_cup.cpp
+++ jug_cup.cpp
@@ -1,3 +1,16 @@
+/*Consider a Jug of capacity L liters. Given N cups of different capacities Ci (in liters), fill the Jug with the help of cups, according the specification.
+The specification according to which the cups may be used to fill the Jug is as below:
+
+* Cups can be used integral number of times i.e., zero or more times, but never partially i.e., a cup of 1L can be used 0, 1, 2 etc. times, but never 0.5, 1.5, 2.5 .. times
+*The Jug must not overflow because of cup filling the Jug
+*The number of distinct cups (i.e., different cup sizes) used to fill the Jug must be maximized
+*The summation of number of times all cups are used must be minimized.
+Consider point 3 to be more important than point 4 when meeting the optimisation goals.
+First line contains an integer N denoting the number of cups available.
+Second line contains N space separated integers denoting the capacity of the cups.
+Third line contains an integer L which denotes the capacity of Jug in liters.*/
+   
+
 #include<iostream>
 using namespace std;
 void calc(int& n,int& jug,int cup[],int & check,int ans[]);
