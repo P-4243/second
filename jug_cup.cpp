30b9f3fbd17161ed55b893684f6ea1c438a8ef20
P-4243 <148359317+P-4243@users.noreply.github.com>
2023-11-01 16:17:01 +0530

    Update jug_cup.cpp

--- jug_cup.cpp
+++ jug_cup.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
+void calc(int& n,int& jug,int cup[],int & check,int ans[]);
 int main()
-{int i,n,cup[4],jug,ans[4],check=0,j;
+{int i,n,cup[4],jug,ans[4],check=0;
    cout<<"enter jug";
     cin>>jug;
 
@@ -14,23 +15,7 @@ int main()
         jug=jug-cup[i];
         ++ans[i];
     }
- for(i=n-1;i>=0;--i)
-    {
-        int enq=jug%cup[i];
-        for(j=n-1;j>=0;--j)
-        {
-            if(enq%cup[j]==0)
-            {
-                check--;
-                ans[j]+=enq/cup[j];
-                ans[i]+=jug/cup[i];
-                break;
-            }
-        }
-       if(check!=0)
-        break;
-    }
-
+calc( n,jug,cup,check,ans);
 
     if(check==0)
     {
@@ -39,10 +24,21 @@ int main()
             jug=jug-cup[i];
             ++ans[i];
         }
-       for(i=n-1;i>=0;--i)
+ calc( n,jug,cup,check,ans);
+    }
+     for(i=0;i<n;++i)
+        {cout<<cup[i]<<" "<<ans[i]<<endl;}
+
+
+
+        return 0;
+
+}
+void calc(int& n, int& jug, int cup[],int& check,int ans[])
+{for(int i=n-1;i>=0;--i)
     {
         int enq=jug%cup[i];
-        for(j=n-1;j>=0;--j)
+        for(int j=n-1;j>=0;--j)
         {
             if(enq%cup[j]==0)
             {
@@ -56,12 +52,5 @@ int main()
         break;
     }
 
-    }
-     for(i=0;i<n;++i)
-        {cout<<cup[i]<<" "<<ans[i]<<endl;}
-
-
-
-        return 0;
 
 }
